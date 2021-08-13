b1a1df55d145c1a7a90169d241747c912e78e0da
Swung0x48 <swung0x48@outlook.com>
2021-08-13 12:45:33 +0800

    feat: BingoCardView

---
+++ src/main/java/info/bcrc/mc/bingo/view/BingoCardView.java
@@ -0,0 +1,36 @@
+package info.bcrc.mc.bingo.view;
+
+import info.bcrc.mc.bingo.Bingo;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class BingoCardView
+{
+    Inventory inventory;
+
+    BingoCardView(Bingo plugin, Player player) {
+        inventory = plugin.getServer().createInventory(null, 45, player.getName() + "'s Bingo Card");
+    }
+
+    public void setup(ItemStack[] items) {
+        inventory.setContents(items);
+    }
+
+    public void toggle(int index) {
+        if (index < 0 || index > inventory.getSize())
+            throw new ArrayIndexOutOfBoundsException("Attempted to toggle an out of bound cell.");
+
+        ItemStack item = new ItemStack(Material.BARRIER);
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) {
+            throw new RuntimeException("Item meta cannot be retrieved.");
+        }
+        meta.setDisplayName(ChatColor.GREEN + "Achieved");
+        item.setItemMeta(meta);
+        inventory.setItem(index, item);
+    }
+}

71013405c6206f4b8af9b337c8c5b4ded5321fc9
Ghomist <ghomist@gmail.com>
2021-08-13 15:44:10 +0800

    structure: impl package structure

--- src/main/java/info/bcrc/mc/bingo/Bingo.java
+++ src/main/java/info/bcrc/mc/bingo/Bingo.java
@@ -1,13 +1,14 @@
 package info.bcrc.mc.bingo;
 
-import java.util.*;
+import java.util.Objects;
 
-import info.bcrc.mc.bingo.controller.BingoCommandExecutor;
-import info.bcrc.mc.bingo.controller.BingoListener;
-import info.bcrc.mc.bingo.base.service.BingoGame;
-import info.bcrc.mc.bingo.util.BingoConfig;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import info.bcrc.mc.bingo.base.service.BingoGame;
+import info.bcrc.mc.bingo.controller.BingoCommandExecutor;
+import info.bcrc.mc.bingo.controller.BingoListener;
+import info.bcrc.mc.bingo.util.BingoConfig;
 
 public class Bingo extends JavaPlugin {
     public boolean setupBingo = false;
@@ -15,8 +16,7 @@ public class Bingo extends JavaPlugin {
     public World bingoWorld;
     private ItemDisplayer itemDisplayer;
 
-    public BingoGame getBingoGame()
-    {
+    public BingoGame getBingoGame() {
         return bingoGame;
     }
 
@@ -25,8 +25,7 @@ public class Bingo extends JavaPlugin {
     private BingoCommandExecutor bingoCommandExecutor;
     private BingoListener bingoListener;
 
-    public BingoConfig getBingoConfig()
-    {
+    public BingoConfig getBingoConfig() {
         return bingoConfig;
     }
 
@@ -41,7 +40,7 @@ public class Bingo extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
         bingoConfig = new BingoConfig(this.getConfig());
-        
+
         saveConfig();
 
         itemDisplayer = new ItemDisplayer(this);

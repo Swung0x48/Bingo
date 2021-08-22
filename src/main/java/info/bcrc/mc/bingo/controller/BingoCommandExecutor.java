bc38391116461c7dc4267b0f97b6cad102d819d9
Xenapte <17661805+Xenapte@users.noreply.github.com>
2021-08-22 12:18:56 +0800

    bingo commands - feat: "viewcard" command - fix: prevent players from joining multiple times

--- src/main/java/info/bcrc/mc/bingo/controller/BingoCommandExecutor.java
+++ src/main/java/info/bcrc/mc/bingo/controller/BingoCommandExecutor.java
@@ -11,7 +11,6 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
 import info.bcrc.mc.bingo.Bingo;
-import info.bcrc.mc.bingo.base.service.BingoGame;
 import info.bcrc.mc.bingo.base.service.BingoGame.GameState;
 import info.bcrc.mc.bingo.impl.classic.service.BingoGameClassic;
 import info.bcrc.mc.bingo.util.MessageSender;
@@ -47,15 +46,20 @@ public class BingoCommandExecutor implements CommandExecutor, TabCompleter {
                         return true;
 
                     if (plugin.getBingoGame() == null) {
-                        sender.sendMessage(MessageSender.bingoPrefix + "Error: No bingo game has been set up.");
+                        sender.sendMessage(MessageSender.bingoErrorPrefix + "No bingo game has been set up.");
                         sender.sendMessage(MessageSender.bingoPrefix + "Use /bingo setup to set one up.");
+                        return true;
+                    }
+
+                    if (plugin.bingoGame.isPlayerInGame((Player) sender)) {
+                        sender.sendMessage(MessageSender.bingoErrorPrefix + "You've already joined the game.");
                         return true;
                     }
 
                     plugin.bingoGame.join((Player) sender);
                     return true;
                 case "start":
-                    if (plugin.bingoGame.getGameState() != BingoGame.GameState.SETUP)
+                    if (plugin.bingoGame.getGameState() != GameState.SETUP)
                         sender.sendMessage(MessageSender.bingoErrorPrefix + "The bingo has not been set up");
                     else
                         plugin.bingoGame.start();
@@ -70,14 +74,14 @@ public class BingoCommandExecutor implements CommandExecutor, TabCompleter {
                         plugin.bingoGame.stop();
                     }
                     else
-                        sender.sendMessage(MessageSender.bingoPrefix + "Error: No bingo game is running currently.");
+                        sender.sendMessage(MessageSender.bingoErrorPrefix + "No bingo game is running currently.");
                     return true;
                 case "quit":
                     if (!isPlayer(sender))
                         return true;
                     
                     if (plugin.bingoGame == null || plugin.bingoGame.getGameState() == GameState.UNINITIALIZED) {
-                        sender.sendMessage(MessageSender.bingoPrefix + "Error: No bingo game has been set up.");
+                        sender.sendMessage(MessageSender.bingoErrorPrefix + "No bingo game has been set up.");
                         return true;
                     }
                     
@@ -88,6 +92,15 @@ public class BingoCommandExecutor implements CommandExecutor, TabCompleter {
                         sender.sendMessage(MessageSender.bingoPrefix + "Type " + ChatColor.GREEN + "/bingo quit confirm" + ChatColor.RESET + " to quit it.");
                     }
                     return true;
+                case "viewcard":
+                    if (!isPlayer(sender))
+                        return true;
+                    
+                    if (plugin.bingoGame.getGameState() == GameState.RUNNING)
+                        plugin.getBingoGame().openBingoCard((Player) sender);
+                    else
+                        sender.sendMessage(MessageSender.bingoErrorPrefix + "You can only view the card during a running bingo game!");
+                    return true;
             }
         }
         return false;
@@ -97,7 +110,7 @@ public class BingoCommandExecutor implements CommandExecutor, TabCompleter {
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         if (command.getName().equalsIgnoreCase("bingo")) {
             if (args.length <= 1)
-                return Arrays.asList("setup", "join", "quit", "start", "status", "stop");
+                return Arrays.asList("setup", "join", "quit", "start", "status", "viewcard", "stop");
             if (args.length > 1 && args[0].equalsIgnoreCase("quit"))
                 return Arrays.asList("confirm");
         }
